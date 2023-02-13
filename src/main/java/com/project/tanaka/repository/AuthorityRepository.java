97c636b2ab8edde4e223a5aa8a3e166c2d71c17f
hoangnguyen <hiep.pd@golineglobal.vn>
2023-02-13 16:56:00 +0700

    Initial version of tanaka generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/project/tanaka/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.project.tanaka.repository;
+
+import com.project.tanaka.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/project/tanaka/service/mapper/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * MapStruct mappers for mapping domain objects and Data Transfer Objects.
+ */
+package com.project.tanaka.service.mapper;
